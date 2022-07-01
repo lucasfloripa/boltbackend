f75976a011ff4ac2427d607403d077903e53323c
Lucas Gonçalves <lucas.floripa@icloud.com>
2022-07-01 19:45:40 -0300

    feat: serverError

--- src/presentation/helpers/http-helpers.ts
+++ src/presentation/helpers/http-helpers.ts
@@ -1,6 +1,12 @@
 import { HttpResponse } from '@/presentation/protocols'
+import { ServerError } from '@/presentation/errors'
 
 export const forbidden = (error: Error): HttpResponse => ({
   statusCode: 403,
   body: error
+})
+
+export const serverError = (error: Error): HttpResponse => ({
+  statusCode: 500,
+  body: new ServerError(error.stack)
 })
