630118ada1ad329256b6784ea85bea567eaa856b
Lucas Gonçalves <lucas.floripa@icloud.com>
2022-07-02 08:47:43 -0300

    feat: ensure RegisterUserController calls validation with correct values

--- src/presentation/controllers/register-user-controller.ts
+++ src/presentation/controllers/register-user-controller.ts
@@ -1,15 +1,17 @@
-import { Controller, HttpResponse } from '@/presentation/protocols'
+import { Controller, HttpResponse, Validation } from '@/presentation/protocols'
 import { EmailInUseError } from '@/presentation/errors'
 import { forbidden, serverError } from '@/presentation/helpers'
 import { RegisterUser } from '@/domain/usecases'
 
 export class RegisterUserController implements Controller {
   constructor (
-    private readonly registerUser: RegisterUser
+    private readonly registerUser: RegisterUser,
+    private readonly validation: Validation
   ) { }
 
   async handle (request: RegisterUserController.Params): Promise<HttpResponse> {
     try {
+      this.validation.validate(request)
       const isValid = await this.registerUser.register(request)
       if (!isValid) {
         return forbidden(new EmailInUseError())
