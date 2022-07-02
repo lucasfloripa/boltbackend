d13124eadc1236597d1d258b5d55ae33ec503b07
Lucas Gonçalves <lucas.floripa@icloud.com>
2022-07-02 15:34:59 -0300

    refactor: update models mock

--- tests/domain/mocks/mock-project.ts
+++ tests/domain/mocks/mock-project.ts
@@ -2,6 +2,7 @@ import { Project } from '@/domain/models'
 import { mockTask } from '@/tests/domain/mocks'
 
 export const mockProject = (): Project => ({
+  id: 'any-id',
   title: 'any_project_title',
   tasks: [mockTask()]
 })
--- tests/domain/mocks/mock-task.ts
+++ tests/domain/mocks/mock-task.ts
@@ -1,6 +1,8 @@
 import { Task } from '@/domain/models'
 
 export const mockTask = (): Task => ({
+  id: 'any-task-id',
+  projectId: 'any-project-id',
   description: 'task_description',
   creationDate: new Date(1995, 11, 17),
   finishDate: new Date(1995, 11, 18),
