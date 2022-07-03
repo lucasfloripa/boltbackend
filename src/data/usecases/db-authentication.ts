5c5ce12c2d76981a74d1ad04d20a557d26533677
Lucas Gonçalves <lucas.floripa@icloud.com>
2022-07-03 15:51:08 -0300

    feat: ensure DbAuthentication works correctly

---
+++ src/data/usecases/db-authentication.ts
@@ -0,0 +1,23 @@
+import { Authenticate } from '@/domain/usecases'
+import { LoadUserByEmailRepository, HashComparer, Encrypter } from '@/data/protocols'
+
+export class DbAuthentication implements Authenticate {
+  constructor (
+    private readonly loadUserByEmailRepository: LoadUserByEmailRepository,
+    private readonly hashComparer: HashComparer,
+    private readonly encrypter: Encrypter
+  ) {}
+
+  async auth (credentials: Authenticate.Params): Promise<string> {
+    const { email, password } = credentials
+    const user = await this.loadUserByEmailRepository.loadByEmail(email)
+    if (user) {
+      const isValid = await this.hashComparer.compare(password, user.password)
+      if (isValid) {
+        const accessToken = await this.encrypter.encrypt(user.id)
+        return accessToken
+      }
+    }
+    return null
+  }
+}
