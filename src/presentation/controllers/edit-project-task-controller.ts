150167aa5ad28887cbe7a09df18b94bfb03e0187
Lucas Gonçalves <lucas.floripa@icloud.com>
2022-07-03 15:21:21 -0300

    feat: ensure EditProjectTaskController works correctly

---
+++ src/presentation/controllers/edit-project-task-controller.ts
@@ -0,0 +1,33 @@
+import { Controller, HttpResponse, Validation } from '@/presentation/protocols'
+import { EditProjectTask } from '@/domain/usecases'
+import { badRequest, serverError, ok, notFound } from '@/presentation/helpers'
+
+export class EditProjectTaskController implements Controller {
+  constructor (
+    private readonly editProjectTask: EditProjectTask,
+    private readonly validation: Validation
+  ) {}
+
+  async handle (request: EditProjectTaskController.Params): Promise<HttpResponse> {
+    try {
+      const error = this.validation.validate(request)
+      if (error) {
+        return badRequest(error)
+      }
+      const isValid = await this.editProjectTask.edit(request)
+      if (!isValid) {
+        return notFound()
+      }
+      return ok({ message: 'Task updated!' })
+    } catch (error) {
+      return serverError(error)
+    }
+  }
+}
+
+export namespace EditProjectTaskController {
+  export type Params = {
+    projectId?: string
+    description?: string
+  }
+}
