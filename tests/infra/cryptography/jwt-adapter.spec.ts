f57d3a1219cc48da4762608632dab8546118779a
Lucas Gonçalves <lucas.floripa@icloud.com>
2022-07-03 22:13:13 -0300

    test: ensure Cryptographys work correctly

---
+++ tests/infra/cryptography/jwt-adapter.spec.ts
@@ -0,0 +1,39 @@
+import { JwtAdapter } from '@/infra/cryptography'
+
+import jwt from 'jsonwebtoken'
+
+jest.mock('jsonwebtoken', () => ({
+  async sign (): Promise<string> {
+    return 'access_token'
+  }
+}))
+
+const makeSut = (): JwtAdapter => {
+  return new JwtAdapter()
+}
+
+describe('Jwt Adapter', () => {
+  describe('encrypt()', () => {
+    test('Should call encrypt with correct values', async () => {
+      const sut = makeSut()
+      const encryptSpy = jest.spyOn(jwt, 'sign')
+      await sut.encrypt('user_id')
+      expect(encryptSpy).toHaveBeenCalledWith({ id: 'user_id' }, 'boltsecret')
+    })
+
+    test('Should throw if sign throws', async () => {
+      const sut = makeSut()
+      jest.spyOn(jwt, 'sign').mockImplementationOnce(async () => {
+        throw new Error()
+      })
+      const promise = sut.encrypt('any_value')
+      await expect(promise).rejects.toThrow()
+    })
+
+    test('Should return a access token on encrypt success', async () => {
+      const sut = makeSut()
+      const accessToken = await sut.encrypt('user_id')
+      expect(accessToken).toBe('access_token')
+    })
+  })
+})
